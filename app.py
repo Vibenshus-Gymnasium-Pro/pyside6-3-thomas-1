0eef36b1e13e8f6be2eff29e3735c1f1b57ee74e
CoolTL <thomasliisborg@gmail.com>
2025-09-15 14:15:52 +0200

    done some stuff

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+import ticktack
 
 from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
@@ -17,39 +18,88 @@ class Tictactoe(QObject):
         self.ui = loader.load("ticktacktoe.ui", None)
         self.turn = 0
         self.btnlist = []
+        self.won = False
+        self.cross = True
+        self.x_won = 0
+        self.o_won = 0
         
         
-        
-        # Creating a list of all the buttons, for use in the for loop connecting them all to the same slot with different parameters. NOTE Not done
+        # Creating a list of all the buttons, for use in the for loop connecting them all to the same slot with different parameters.
         self.buttons = [self.ui.button1, self.ui.button2, self.ui.button3, self.ui.button4, self.ui.button5, self.ui.button6, self.ui.button7, self.ui.button8, self.ui.button9]
         for a in self.buttons:
             a.clicked.connect(lambda checked, val=a : self.pressed(val))
+        self.update_status()
+        
+        
         
     
 
     def pressed(self, button):
+        # checks if all buttons have been pressed, if so clears
+        if self.won == True:
+            self.clear
+            self.won = False
+            return
         if len(self.btnlist) >= 9:
             self.clear()
             return
+        
+        
+        
         print("Worked")
         print(button)
+        # Checks if button has been pressed or not
         if button not in self.btnlist:
-            if self.turn % 2 == 0:
+            # Checks whose turn it is, and then writes the coresponding symbol
+            if self.cross:
                 button.setText("X")
-                self.btnlist.append(button)
-                self.turn += 1
-            elif self.turn %2 == 1:
+                
+                
+            else:
                 button.setText("O")
-                self.btnlist.append(button)
-                self.turn += 1
+                
+            self.btnlist.append(button)
+            self.cross = not self.cross
+            self.update_status()
+           
+                
+        
+
+    def update_status(self):
+        if self.cross:
+            self.ui.win.setText("X turn")
+        else:
+            self.ui.win.setText("O turn")
+        if ticktack.check_win() == "O":
+            self.ui.win.setText("O Won!")
+            self.o_won += 1
+            self.won = True
+
+        if ticktack.check_win() == "X":
+            self.ui.win.setText("X Won!")
+            self.x_won += 1
+            self.won = True
+        if len(self.btnlist) >= 9:
+            self.ui.win.setText("Tie")
+        
+
+        self.ui.O_score.setText(f"O Wins: {self.o_won}")
+        self.ui.X_score.setText(f"X Wins: {self.x_won}")
 
     def clear (self):
-
+        # Loops through the buttons, clearing them
         for i in self.btnlist:
             i.setText("")
         self.btnlist = []
+        ticktack.reset()
+        if self.cross:
+            self.ui.win.setText("X turn")
+        else:
+            self.ui.win.setText("O turn")
         
-
+    
+    
+        
 
 
 program = QApplication.instance()
--- ticktack.py
+++ ticktack.py
@@ -1,17 +1,37 @@
 
+X = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+O = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-turn = 0
-
-def tick_or_tack(turn, button):
-    if turn%2 == 0:
-        change_button(button, "X")
-        turn += 1
-    else:
-        change_button(button, "O")
-        turn += 1
-
-def change_button(button, symbol):
+WINS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
 
 
-lambda checked: self.handle_trigger(checked, turn)
+
+def reset():
+    X = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    O = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+def check_win():
+    
+    for win in WINS:
+        index = 0
+        X_hits = 0
+        O_hits = 0
+        for spot in win:
+            if X[index] and spot:
+                X_hits += 1
+            if O[index] and spot:
+                O_hits += 1
+            
+            if X_hits >= 3:
+                return "X"
+            
+            if O_hits >= 3:
+                return "O"
+
+        index += 1
+
+                
+
+
+check_win()
